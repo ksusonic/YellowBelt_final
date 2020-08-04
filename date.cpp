0350e3b260b2a31f042795260413c753b2b778bc
Daniil Moiseev <ksusonic@yandex.ru>
2020-08-04 03:31:57 +0300

    v2.0 Good Job message

--- date.cpp
+++ date.cpp
@@ -16,39 +16,41 @@ Date::Date(int new_year, int new_month, int new_day)
         : year(new_year), month(new_month), day(new_day) {}
 
 string Date::GetString() const {
-    return to_string(year) + '-' + to_string(month) + '-' + to_string(day);
+    stringstream ss;
+    ss << *this;
+    return ss.str();
 }
 
 ostream &operator<<(ostream &os, const Date &date) {
     os << setw(4) << setfill('0') << date.GetYear() <<
-       '-' << setw(2) << setfill('0') << date.GetMonth() <<
-       '-' << setw(2) << setfill('0') << date.GetDay();
+       '-' << setfill('0') << setw(2) << date.GetMonth() <<
+       '-' << setfill('0') << setw(2) << date.GetDay();
     return os;
 }
 
 bool operator<(const Date &lhs, const Date &rhs) {
-    return vector<int>{lhs.GetYear(), lhs.GetMonth(), lhs.GetDay()} <
-           vector<int>{rhs.GetYear(), rhs.GetMonth(), rhs.GetDay()};
+    return make_tuple(lhs.GetYear(), lhs.GetMonth(), lhs.GetDay()) <
+            make_tuple(rhs.GetYear(), rhs.GetMonth(), rhs.GetDay());
 }
 bool operator<=(const Date &lhs, const Date &rhs) {
-    return vector<int>{lhs.GetYear(), lhs.GetMonth(), lhs.GetDay()} <=
-           vector<int>{rhs.GetYear(), rhs.GetMonth(), rhs.GetDay()};
+    return make_tuple(lhs.GetYear(), lhs.GetMonth(), lhs.GetDay()) <=
+           make_tuple(rhs.GetYear(), rhs.GetMonth(), rhs.GetDay());
 }
 bool operator>(const Date &lhs, const Date &rhs) {
-    return vector<int>{lhs.GetYear(), lhs.GetMonth(), lhs.GetDay()} >
-           vector<int>{rhs.GetYear(), rhs.GetMonth(), rhs.GetDay()};
+    return make_tuple(lhs.GetYear(), lhs.GetMonth(), lhs.GetDay()) >
+           make_tuple(rhs.GetYear(), rhs.GetMonth(), rhs.GetDay());
 }
 bool operator>=(const Date &lhs, const Date &rhs) {
-    return vector<int>{lhs.GetYear(), lhs.GetMonth(), lhs.GetDay()} >=
-           vector<int>{rhs.GetYear(), rhs.GetMonth(), rhs.GetDay()};
+    return make_tuple(lhs.GetYear(), lhs.GetMonth(), lhs.GetDay()) >=
+           make_tuple(rhs.GetYear(), rhs.GetMonth(), rhs.GetDay());
 }
 bool operator==(const Date &lhs, const Date &rhs) {
-    return vector<int>{lhs.GetYear(), lhs.GetMonth(), lhs.GetDay()} ==
-           vector<int>{rhs.GetYear(), rhs.GetMonth(), rhs.GetDay()};
+    return make_tuple(lhs.GetYear(), lhs.GetMonth(), lhs.GetDay()) ==
+           make_tuple(rhs.GetYear(), rhs.GetMonth(), rhs.GetDay());
 }
 bool operator!=(const Date &lhs, const Date &rhs) {
-    return vector<int>{lhs.GetYear(), lhs.GetMonth(), lhs.GetDay()} !=
-           vector<int>{rhs.GetYear(), rhs.GetMonth(), rhs.GetDay()};
+    return make_tuple(lhs.GetYear(), lhs.GetMonth(), lhs.GetDay()) !=
+           make_tuple(rhs.GetYear(), rhs.GetMonth(), rhs.GetDay());
 }
 
 Date ParseDate(istream &in) {
